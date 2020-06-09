03d59de07381adf6959c41a2f0bc63ac06518691
Rohith Goud <mrohithg@gmail.com>
2020-06-09 10:05:16 +0530

    added dev.js

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports={
+    googleClientID:'279896299448-l9js5jdbj05dum6ltiuh3uu2bb72hmfn.apps.googleusercontent.com',
+    googleClientSecret:'70BhXXx3GiYgwD9Ucy9e7jJC',
+    mongoURI:'mongodb+srv://rohith:rohith@mvcomcluster-rqhsh.mongodb.net/wishlist?retryWrites=true&w=majority',
+    cookieKey:'asdf',
+    FACEBOOK_APP_ID:process.env.FACEBOOK_APP_ID,
+    FACEBOOK_APP_SECRET:process.env.FACEBOOK_APP_SECRET
+}
