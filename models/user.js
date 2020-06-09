a91d99542ca76bfacfd24cdef4946b99a7a13857
Rohith Goud <mrohithg@gmail.com>
2020-06-09 10:02:29 +0530

    added user.js

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email:String,
+    username:String,
+    picture:String,
+    googleProviderId: String,
+    name: String,
+    createdAt: {type:Date, default: Date.now},
+    lastLogin: {type:Date, default: Date.now},
+	seenMovies:[String]
+});
+
+module.exports = mongoose.model("User",userSchema);
