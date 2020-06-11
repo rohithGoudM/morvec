1a329f8c7ae1ab9303bb0e799b150465a44db2fb
Rohith Goud <mrohithg@gmail.com>
2020-06-11 19:34:51 +0530

    dev db and morvec db

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,9 @@
+const dbname = process.env.NODE_ENV === 'production' ? 'morvec' : 'dev';
+
 module.exports={
     googleClientID:'279896299448-l9js5jdbj05dum6ltiuh3uu2bb72hmfn.apps.googleusercontent.com',
     googleClientSecret:'70BhXXx3GiYgwD9Ucy9e7jJC',
-    mongoURI:'mongodb+srv://rohith:rohith@mvcomcluster-rqhsh.mongodb.net/wishlist?retryWrites=true&w=majority',
+    mongoURI:'mongodb+srv://rohith:rohith@mvcomcluster-rqhsh.mongodb.net/'+dbname+'?retryWrites=true&w=majority',
     cookieKey:'asdf',
     FACEBOOK_APP_ID:process.env.FACEBOOK_APP_ID,
     FACEBOOK_APP_SECRET:process.env.FACEBOOK_APP_SECRET
