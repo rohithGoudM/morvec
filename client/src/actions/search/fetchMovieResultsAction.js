a5dce981b00b6f7e2ee99c3050fc17d708b0b0b8
Rohith Goud <mrohithg@gmail.com>
2020-07-08 21:08:26 +0530

    Too many results, error fixed with title option

--- client/src/actions/search/fetchMovieResultsAction.js
+++ client/src/actions/search/fetchMovieResultsAction.js
@@ -1,19 +1,48 @@
 export const fetchMovieResultsAction = (query)=>{
    return (dispatch)=>{
-
-   	fetch('http://www.omdbapi.com/?s='+query.trim().split(" ").join('+')+'&apikey=b36aa754')
-    .then(res=>res.json())
-    .then(results=>{
-      if(results.Response == "True"){
-      	dispatch({type:'SET_MOVIE_RESULTS',payload: results["Search"].filter((it)=>{
-          return it.Type == "movie" || it.Type == "series";
-        })});
-      }else{
-      	dispatch({type:'PUSH_ERROR_FROM_MOVIE',payload:{
-          Poster:"./NA.jpg",
-          Title:"Error",Year:results.Error
-        }});
-      }
-    });
+    if(query.trim().length <= 3){
+      fetch('http://www.omdbapi.com/?t='+query.trim().split(" ").join('+')+'&apikey=b36aa754')
+      .then(ress=>ress.json())
+      .then(result=>{
+        if(result.Response == "True" && (result.Type == "movie" || result.Type == "series")){
+          dispatch({type:'SET_MOVIE_RESULTS',payload:[result]});
+        }else{
+          dispatch({type:'PUSH_ERROR_FROM_MOVIE',payload:{
+            Poster:"./NA.jpg",
+            Title:"Error",Year:result.Error
+          }});
+        }
+      })
+    }else{
+     	fetch('http://www.omdbapi.com/?s='+query.trim().split(" ").join('+')+'&apikey=b36aa754')
+      .then(res=>res.json())
+      .then(results=>{
+        if(results.Response == "True"){
+        	dispatch({type:'SET_MOVIE_RESULTS',payload: results["Search"].filter((it)=>{
+            return it.Type == "movie" || it.Type == "series";
+          })});
+        }else{
+          if(results.Error == "Too many results."){
+            fetch('http://www.omdbapi.com/?t='+query.trim().split(" ").join('+')+'&apikey=b36aa754')
+            .then(ress=>ress.json())
+            .then(result=>{
+              if(result.Response == "True" && (result.Type == "movie" || result.Type == "series")){
+                dispatch({type:'SET_MOVIE_RESULTS',payload:[result]});
+              }else{
+                dispatch({type:'PUSH_ERROR_FROM_MOVIE',payload:{
+                  Poster:"./NA.jpg",
+                  Title:"Error",Year:result.Error
+                }});
+              }
+            });
+          }else{
+            dispatch({type:'PUSH_ERROR_FROM_MOVIE',payload:{
+              Poster:"./NA.jpg",
+              Title:"Error",Year:results.Error
+            }});
+          }      	
+        }
+      });
+    }
    }
 }
